d1e3e534a5b227d30941f666e9eadc0d6448d983
shehabic <shehabic@gmail.com>
2018-10-19 01:26:20 +0200

    Removing pods

--- .ios/Pods/Headers/Public/dhplugin/DhpluginPlugin.h
+++
@@ -1,1 +0,0 @@
-../../../../Flutter/.symlinks/dhplugin/ios/Classes/DhpluginPlugin.h
--- .ios/Pods/Target Support Files/dhplugin/dhplugin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_dhplugin : NSObject
-@end
-@implementation PodsDummy_dhplugin
-@end
